47079accf9cdcfce221a2d036310a60438bf2383
mikzanco <mikyzancolo97@gmail.com>
2022-10-26 17:28:40 +0200

    add img, css and html part

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+1 ricerare container immagine in html
+2 creare array con immagine titolo e descrizione
+3 creare thumbnail sotto l'immagine principale
+4 l'immagine attiva sarà illimunata a colori normali e rialzata con un bordo bianco.
+5 le altre immagini scure a comporre tutta la linea.
+6 creare due bottono uno avanti e uno indietro.
+*/
