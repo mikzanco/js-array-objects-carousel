f2583052260af755555fa5d52acba4217edb300b
mikzanco <mikyzancolo97@gmail.com>
2022-11-10 00:16:15 +0100

    add e fix loop carousel: con handleNextPrev al click dei tasti vado a spostare il active sull'immagine prcedente o successiva

--- js/script.js
+++ js/script.js
@@ -45,8 +45,16 @@ const images = [
 
 const containerImages = document.querySelector('.my-carousel-images');
 const containerThumb = document.querySelector('.my-thumbnails .inner');
+const nextBtn = document.querySelector('.my-next-hook');
+const prevBtn = document.querySelector('.my-prev-hook');
 
-const counterImages = 0;
+nextBtn.isNext = true;
+prevBtn.isNext = false;
+
+nextBtn.addEventListener('click', handleNextPrev);
+prevBtn.addEventListener('click', handleNextPrev);
+
+let counterImages = 0;
 
 init();
 
@@ -66,8 +74,27 @@ function init(){
     console.log('init');
 }
 
+function handleNextPrev(){
+    deactivateImage();
+    if(this.isNext){
+        counterImages++;
+        if(counterImages === images.length) counterImages = 0;
+    }else{
+        counterImages--;
+        if(counterImages < 0) counterImages = images.length -1;
+    }
+    activateImage();
+
+}
+
 function activateImage(){
     document.getElementsByClassName('my-carousel-item ')[counterImages].classList.add('active');
+    document.getElementsByClassName('my-thumbnail')[counterImages].classList.add('active');
+}
+
+function deactivateImage(){
+    document.getElementsByClassName('my-carousel-item ')[counterImages].classList.remove('active');
+    document.getElementsByClassName('my-thumbnail')[counterImages].classList.remove('active');
 }
 
 function getTemplateImage(image){
@@ -88,7 +115,7 @@ function getTemplateThumb(image){
     const {immagine, paese} = image;
 
     return `
-    <div class="my-thumbnail active">
+    <div class="my-thumbnail ">
         <img class="img-fluid" src="${immagine}" alt="Thumbnail of ${paese} picture">
     </div>
     `
