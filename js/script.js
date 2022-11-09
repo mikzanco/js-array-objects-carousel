d540a668b7366ed42c8be9f0a52f9f3b9b908117
mikzanco <mikyzancolo97@gmail.com>
2022-11-09 23:50:03 +0100

    fix problem img, create templateImg con template litteral, add a ciclo forEach aggiunto classe active per mostare l'immagine.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,96 @@
 1 ricerare container immagine in html
 2 creare array con immagine titolo e descrizione
 3 creare thumbnail sotto l'immagine principale
+----------------------------------------
+
+
 4 l'immagine attiva sarà illimunata a colori normali e rialzata con un bordo bianco.
 5 le altre immagini scure a comporre tutta la linea.
 6 creare due bottono uno avanti e uno indietro.
 */
+// const container = document.querySelector('.container');
+// const slider = document.querySelector('.slider');
+// const thumbs = document.querySelector('.thumbs');
+
+
+
+const images = [
+    {
+        paese: 'Argentina',
+        commento: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        immagine: 'img/argentina.jpg',
+    },
+    {
+        paese: 'Chile',
+        commento: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        immagine: 'img/chile.jpg',
+    },
+    {
+        paese: 'Colombia',
+        commento: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        immagine: 'img/colombia.jpg',
+    },
+    {
+        paese: 'Perù',
+        commento: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        immagine: 'img/peru.jpg',
+    },
+    {
+        paese: 'Sweden',
+        commento: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        immagine: 'img/sweden.jpg',
+    },
+];
+
+const containerImages = document.querySelector('.my-carousel-images');
+const containerThumb = document.querySelector('.my-thumbnails .inner');
+
+const counterImages = 0;
+
+init();
+
+function init(){
+
+    // crep gli elementi per svuotare tutto.
+    containerImages.innerHTML = '';
+    containerThumb.innerHTML = '';
+
+    // ciclo l'array di immagini e stampo in pagina e immagini
+
+    images.forEach(image => {
+        containerImages.innerHTML += getTemplateImage(image);
+        containerThumb.innerHTML += getTemplateThumb(image);
+    });
+    activateImage();
+    console.log('init');
+}
+
+function activateImage(){
+    document.getElementsByClassName('my-carousel-item ')[counterImages].classList.add('active');
+}
+
+function getTemplateImage(image){
+    const {immagine, paese, commento} = image;
+
+    return `
+    <div class="my-carousel-item" >
+        <img class="img-fluid" src="${immagine}" alt="${paese}picture">
+        <div class="item-description px-3">
+            <h2>${paese}</h2>
+            <p>${commento}</p>
+        </div>
+    </div>
+    `
+}
+
+function getTemplateThumb(image){
+    const {immagine, paese} = image;
+
+    return `
+    <div class="my-thumbnail active">
+        <img class="img-fluid" src="${immagine}" alt="Thumbnail of ${paese} picture">
+    </div>
+    `
+}
+
+
